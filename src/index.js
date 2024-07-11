162deee78f3bdd9b259abe8d08e9dc1b31346aab
WANGZeping <79504116+zepingwong@users.noreply.github.com>
2024-07-11 17:24:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.dicode.top": dockerHub,
+  "quay.dicode.top": "https://quay.io",
+  "gcr.dicode.top": "https://gcr.io",
+  "k8s-gcr.dicode.top": "https://k8s.gcr.io",
+  "k8s.dicode.top": "https://registry.k8s.io",
+  "ghcr.dicode.top": "https://ghcr.io",
+  "cloudsmith.dicode.top": "https://docker.cloudsmith.io",
+  "ecr.dicode.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.dicode.top": dockerHub,
 };
 
 function routeByHosts(host) {
